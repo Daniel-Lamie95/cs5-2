cafc125185ae012ab7fdcd90144442eb88b72696
Daniel-Lamie95 <danielseddik95@gmail.com>
2026-04-13 16:56:58 +0200

    fixed student db

--- src/main/java/com/example/apa_project/StudentDB.java
+++ src/main/java/com/example/apa_project/StudentDB.java
@@ -6,39 +6,36 @@ public class StudentDB {
     private String constr = "null";
 
     public StudentDB(){
-        Connection con = null;
-        try{
+        loadDriver();
+        createStudentsTable();
+    }
+
+    private void loadDriver() {
+        try {
             // load driver
             Class.forName("null");
-
-            con = DriverManager.getConnection(constr);
-            Statement stmt = con.createStatement();
-
-            String createtable = "CREATE TABLE IF NOT EXISTS students (" +
-                    "id INT PRIMARY KEY AUTO_INCREMENT," +
-                    "name VARCHAR(255) NOT NULL," +
-                    "email VARCHAR(255) NOT NULL UNIQUE," +
-                    "password VARCHAR(255) NOT NULL," +
-                    "major VARCHAR(255) NOT NULL," +
-                    "date_of_birth DATE NOT NULL," +
-                    "cv_file_name VARCHAR(255)," +
-                    "cv_document BLOB" +
-                    ")";
-
-            stmt.executeUpdate(createtable);
-
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void createStudentsTable() {
+        String createTable = "CREATE TABLE IF NOT EXISTS students (" +
+                "id INT PRIMARY KEY AUTO_INCREMENT," +
+                "name VARCHAR(255) NOT NULL," +
+                "email VARCHAR(255) NOT NULL UNIQUE," +
+                "password VARCHAR(255) NOT NULL," +
+                "major VARCHAR(255) NOT NULL," +
+                "date_of_birth DATE NOT NULL," +
+                "cv_file_name VARCHAR(255)," +
+                "cv_document BLOB" +
+                ")";
+
+        try (Connection con = DriverManager.getConnection(constr);
+             Statement stmt = con.createStatement()) {
+            stmt.executeUpdate(createTable);
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }finally {
-            if (con != null){
-                try{
-                    con.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
     }
 
