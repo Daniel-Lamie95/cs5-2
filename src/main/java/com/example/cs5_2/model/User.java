730eb3b5445b04626f9270e6365040e744d0a4ee
Alaa Ahmed <amegahed874@gmail.com>
2026-04-28 21:42:50 +0300

    Complete Thymeleaf integration for all HTML templates and update User entity

--- src/main/java/com/example/cs5_2/model/User.java
+++ src/main/java/com/example/cs5_2/model/User.java
@@ -2,19 +2,33 @@ package com.example.cs5_2.model;
 
 import jakarta.persistence.*;
 
-@MappedSuperclass
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
     private String email;
     private String password;
+    
+    private String role;
 
     public User() {
     }
 
-    public User(String name, String email, String password) {
+    public User(String name, String email, String password, String role ) {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.role = role;
+        
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
@@ -39,5 +53,12 @@ public abstract class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
